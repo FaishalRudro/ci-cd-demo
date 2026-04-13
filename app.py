b64525025cf8472f37dc8b42519232696efbfea8
FaishalRudro <md.faishalrudro@gmail.com>
2026-04-13 10:01:51 +0600

    add real tests with pytest

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello CI/CD") 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("App is running!")
+print("2 + 3 =", add(2, 3))
+print("10 - 4 =", subtract(10, 4))
+print("3 x 5 =", multiply(3, 5))
